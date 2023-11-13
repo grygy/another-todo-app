e056b075f9ad8285f1fbf42f87649f7641db7bf3
Adam Grygar <grygarad@gmail.com>
2023-11-13 23:29:51 +0100

    feat: add get todos for current user (#8)

--- todo_service/routers/todo.py
+++ todo_service/routers/todo.py
@@ -1,8 +1,10 @@
-import uuid
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from auth.auth import get_current_active_user
+from db.connection import Database, engine
+from db.todo_repository import TodoRepository
 from schemas.todo import TodoSchema
 
 router = APIRouter(
@@ -11,10 +13,11 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-fake_todos_db = [{"id": uuid.UUID("bd65600d-8669-4903-8a14-af88203add38"), "title": "Foo", "description": "Fighters"}]
-
 
 @router.get("/", response_model=List[TodoSchema])
-def get_todos():
-    """Get all todos"""
-    return fake_todos_db
+def get_todos(
+        current_user=Depends(get_current_active_user)
+):
+    """Get all todos for current user"""
+    todo_repository = TodoRepository(Database.get_instance(engine=engine))
+    return todo_repository.get_todos_for_user(current_user.id)
--- todo_service/tests/routers/test_todo.py
+++ todo_service/tests/routers/test_todo.py
@@ -1,22 +1,52 @@
 import unittest
+from unittest.mock import patch
+from uuid import uuid4
 
-from fastapi.encoders import jsonable_encoder
 from fastapi.testclient import TestClient
 
+from auth.auth import get_current_active_user
 from main import app
-from routers.todo import fake_todos_db
+from schemas.todo import TodoSchema
+
+
+class MockUser:
+    def __init__(self, id):
+        self.id = id
 
 
 class TestTodoRoute(unittest.TestCase):
 
     def setUp(self):
-        self.client = TestClient(app)
+        test_user_id = uuid4()
 
-    def test_read_todo(self):
+        def ignore_auth():
+            return MockUser(test_user_id)
+
+        app.dependency_overrides[get_current_active_user] = ignore_auth
+        self.client = TestClient(app)
+        self.test_user_id = test_user_id
+
+    @patch('db.todo_repository.TodoRepository.get_todos_for_user')
+    def test_read_todo(self, mock_get_todos_for_user):
         """Test the GET /todo endpoint"""
+
+        todo_id = uuid4()
+        todos = [
+            TodoSchema(id=todo_id, title="Todo 1", description="Description 1", user_id=self.test_user_id)
+        ]
+        mock_get_todos_for_user.return_value = todos
+        expected_json = [{
+            "id": str(todo_id),
+            "title": "Todo 1",
+            "description": "Description 1",
+        }]
+
         response = self.client.get("/todo")
+
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), jsonable_encoder(fake_todos_db))
+        print(response.json())
+        self.assertEqual(response.json(), expected_json)
+        mock_get_todos_for_user.assert_called_once_with(self.test_user_id)
 
 
 if __name__ == '__main__':
